f3eb676c789e6bf8c54fe413b4dbceb304de25e2
egemen-c <egemen.neverwinter@hotmail.com>
2022-02-22 22:32:24 +0300

    adding new project and Refactor

--- number-generator/dom-mini-project-1/app.js
+++ number-generator/dom-mini-project-1/app.js
@@ -1,6 +1,25 @@
+
+let btn = document.querySelector('#btn')
+btn.addEventListener('click',   function () {
+    let input = document.getElementById('input');
+    let span = document.getElementById('span');
+    let value =  parseInt(input.value, 10)
+    if (isNaN(value)) {
+        alert("lütfen sadece numara girin")       
+    }else {
+        numberGen(value)
+        asal()
+
+    }
+    console.log(isNaN(value));
+})
+
 const container = document.querySelector('#number-wrapper')
-function numberGen() {
-    for (let i = 0; i <= 101; i++ ){
+function numberGen(value) {
+    while(container.lastChild){
+        container.lastChild.remove()
+    }
+    for (let i = 1; i <= value; i++ ){
         const newDiv = document.createElement("div");
         if (i % 2 == 0) {
             newDiv.innerHTML = i 
@@ -12,28 +31,23 @@ function numberGen() {
         }
         container.append(newDiv)
     }
+
 }
 function asal() {
     const divs = document.querySelectorAll('div')
     divs.forEach(element => {
         let isPrime = true;
         let number = parseInt(element.innerHTML)
-    if (number === 1) {
-        console.log("1 is neither prime nor composite number.");
-    }
-    else if (number > 1) {
+        if (number > 1) {
         for (let i = 2; i < number; i++) {
             if (number % i == 0) {
                 isPrime = false;
                 break;
             }
         }
-    
         if (isPrime) {
             element.style.backgroundColor = "red"
         } 
     }
     }) 
 }
-numberGen()
-asal()
